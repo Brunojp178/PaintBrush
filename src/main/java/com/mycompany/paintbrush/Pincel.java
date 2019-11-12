0bc9b8eecf1b5800961d1dca7a27e2cb743d3f2f
BrunoJP <brunojp178@gmail.com>
2019-11-12 17:11:37 -0300

    Commit inicial

---
+++ src/main/java/com/mycompany/paintbrush/Pincel.java
@@ -0,0 +1,13 @@
+package com.mycompany.paintbrush;
+
+import java.awt.Graphics;
+
+public class Pincel {
+    public int tamanho;
+    public String figura;
+    public Graphics canvas;
+    
+    
+    // Methods
+    
+}
